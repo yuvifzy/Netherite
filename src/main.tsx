301740c645178a748673ccbb43ad4289072f8a8e
yuvifzy <yuvrajmohana@users.noreply.github.com>
2026-02-22 02:47:07 +0530

    feat(routing): hash-route to FileDrop component when ?window=filedrop

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import FileDrop from "./FileDrop";
+
+const params = new URLSearchParams(window.location.search);
+const isDropWindow = params.get("window") === "filedrop";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {isDropWindow ? <FileDrop /> : <App />}
   </React.StrictMode>,
 );
